11da7488f37257d40ca303c3070e2a1294a9c040
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-10-03 17:32:18 +1300

    Added Wrapper Plugin

---
+++ TribulaPlugin/src/main/java/io/github/techno_brony/tribula/plugin/BukkitTemplate/TribulaPlugin.java
@@ -0,0 +1,16 @@
+package io.github.techno_brony.tribula.plugin.BukkitTemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TribulaPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
--- TribulaSQL/src/main/java/io/github/techno_brony/tribula/sql/Main.java
+++ TribulaSQL/src/main/java/io/github/techno_brony/tribula/sql/Main.java
@@ -1,17 +1,29 @@
 package io.github.techno_brony.tribula.sql;
 
+import io.github.techno_brony.tribula.plugin.BukkitTemplate.TribulaPlugin;
 import io.github.techno_brony.tribula.sql.commands.CommandSQL;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class Main extends JavaPlugin {
+public class Main extends TribulaPlugin {
+
+    private Connection connection;
+    private String host, database, username, password;
+    private int port;
 
     @Override
     public void onEnable() {
         createAndLoadConfig();
-        getServer().getPluginManager().registerEvents(new EventListener(this), this);
-        getCommand("templatecmd").setExecutor(new CommandSQL(this));
+        parseConfig();
+        try {
+            openSQLConnection();
+        } catch (SQLException | ClassNotFoundException e) {
+            getLogger().severe("Unable to connect to database. Please check your config.");
+        }
+        getCommand("sql").setExecutor(new CommandSQL(this));
     }
 
     @Override
@@ -39,5 +51,24 @@ public class Main extends JavaPlugin {
     }
 
     private void parseConfig() {
+        host = getConfig().getString("host");
+        port = getConfig().getInt("port");
+        database = getConfig().getString("database");
+        username = getConfig().getString("username");
+        password = getConfig().getString("password");
+    }
+
+    public void openSQLConnection() throws SQLException, ClassNotFoundException {
+        if (connection != null && !connection.isClosed()) {
+            return;
+        }
+
+        synchronized (this) {
+            if (connection != null && !connection.isClosed()) {
+                return;
+            }
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database, this.username, this.password);
+        }
     }
 }
