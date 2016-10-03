7da60c04aa48863a955782e49139b9719ccd7395
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-10-03 18:51:25 +1300

    Added getData methods

--- TribulaPlugin/src/main/java/io/github/techno_brony/tribula/plugin/BukkitTemplate/TribulaPlugin.java
+++ TribulaPlugin/src/main/java/io/github/techno_brony/tribula/plugin/BukkitTemplate/TribulaPlugin.java
@@ -2,6 +2,8 @@ package io.github.techno_brony.tribula.plugin.BukkitTemplate;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class TribulaPlugin extends JavaPlugin {
 
     @Override
@@ -10,6 +12,25 @@ public class TribulaPlugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
+
+    }
+
+    protected void createAndLoadConfig() {
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                getLogger().info("Creating config.yml ...");
+                saveDefaultConfig();
+            } else {
+                getLogger().info("Loading config.yml ...");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
 
     }
 
--- TribulaSQL/src/main/java/io/github/techno_brony/tribula/sql/Main.java
+++ TribulaSQL/src/main/java/io/github/techno_brony/tribula/sql/Main.java
@@ -2,11 +2,11 @@ package io.github.techno_brony.tribula.sql;
 
 import io.github.techno_brony.tribula.plugin.BukkitTemplate.TribulaPlugin;
 import io.github.techno_brony.tribula.sql.commands.CommandSQL;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class Main extends TribulaPlugin {
 
@@ -26,30 +26,6 @@ public class Main extends TribulaPlugin {
         getCommand("sql").setExecutor(new CommandSQL(this));
     }
 
-    @Override
-    public void onDisable() {
-
-    }
-
-    private void createAndLoadConfig() {
-        try {
-            if (!getDataFolder().exists()) {
-                getDataFolder().mkdirs();
-            }
-            File file = new File(getDataFolder(), "config.yml");
-            if (!file.exists()) {
-                getLogger().info("Creating config.yml ...");
-                saveDefaultConfig();
-            } else {
-                getLogger().info("Loading config.yml ...");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        }
-
-    }
-
     private void parseConfig() {
         host = getConfig().getString("host");
         port = getConfig().getInt("port");
@@ -58,7 +34,7 @@ public class Main extends TribulaPlugin {
         password = getConfig().getString("password");
     }
 
-    public void openSQLConnection() throws SQLException, ClassNotFoundException {
+    private void openSQLConnection() throws SQLException, ClassNotFoundException {
         if (connection != null && !connection.isClosed()) {
             return;
         }
@@ -70,5 +46,76 @@ public class Main extends TribulaPlugin {
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database, this.username, this.password);
         }
+    }
+
+    /**
+     * Gets a row of data
+     * Guaranteed to only be one row
+     *
+     * @param client   The plugin
+     * @param player   The player to get data from
+     * @param callback A callback which uses the data
+     */
+    public void getData(final TribulaSQLClient client, final UUID player, final TribulaSQLCallback callback) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    openSQLConnection();
+                    Statement statement = connection.createStatement();
+                    ResultSet result = statement.executeQuery(
+                            "SELECT * FROM " + client.getUniqueTableID() + " WHERE UUID = '" + player + "';");
+                    if (result.next()) {
+                        ResultSetMetaData metaData = result.getMetaData();
+                        HashMap<String, Object> data = new HashMap<>();
+                        for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                            data.put(metaData.getColumnName(i), result.getObject(i));
+                        }
+                        callback.done(data);
+                    }
+                } catch (ClassNotFoundException | SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.runTaskAsynchronously(this);
+    }
+
+    /**
+     * Gets a single field of data
+     *
+     * @param client   The plugin
+     * @param player   The player to get the field from
+     * @param callback A callback which uses the field
+     * @param column   The column name
+     */
+    public void getData(final TribulaSQLClient client, final UUID player, final String column, final TribulaSQLCallback callback) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                getData(client, player, new TribulaSQLCallback() {
+                    @Override
+                    public void done(HashMap<String, Object> o) {
+                        HashMap<String, Object> field = new HashMap<>();
+                        field.put(column, o.get(column));
+                        callback.done(field);
+                    }
+                });
+            }
+        }.runTaskAsynchronously(this);
+    }
+
+    public void setData(final TribulaSQLClient client, final UUID player, final String column, final Object value) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    openSQLConnection();
+                    Statement statement = connection.createStatement(); //TODO FINISH
+                    statement.executeUpdate("INSERT INTO " + client.getUniqueTableID() + " (PLAYERNAME, BALANCE) VALUES ('Playername', 100);");
+                } catch (SQLException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.runTaskAsynchronously(this);
     }
 }
