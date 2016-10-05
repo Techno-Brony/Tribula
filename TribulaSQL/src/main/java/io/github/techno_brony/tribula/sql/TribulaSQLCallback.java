27d92f7ba210c3c59ffff05ffaac7c5343ae7eaf
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-10-06 11:18:43 +1300

    STARTING FROM SCRATCH

--- TribulaSQL/src/main/java/io/github/techno_brony/tribula/sql/TribulaSQLCallback.java
+++
@@ -1,7 +0,0 @@
-package io.github.techno_brony.tribula.sql;
-
-import java.util.HashMap;
-
-public interface TribulaSQLCallback {
-    void done(HashMap<String, Object> o);
-}
--- TribulaSQL/src/main/java/io/github/techno_brony/tribula/sql/TribulaSQLClient.java
+++
@@ -1,5 +0,0 @@
-package io.github.techno_brony.tribula.sql;
-
-public interface TribulaSQLClient {
-    String getUniqueTableID();
-}
