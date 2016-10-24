1696dddd96ac636a71ad5eafbb6f389abe76496e
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-10-24 14:14:12 +1300

    Fixed Mob Ambiguous Functions

--- TribulaServerParent/TribulaServer/src/main/java/io/github/techno_brony/tribula/internals/interfaces/ITribulaMob.java
+++ TribulaServerParent/TribulaServer/src/main/java/io/github/techno_brony/tribula/internals/interfaces/ITribulaMob.java
@@ -4,10 +4,10 @@ import io.github.techno_brony.tribula.internals.enums.TribulaMobTrigger;
 import org.bukkit.entity.LivingEntity;
 
 public interface ITribulaMob {
-    String getName();
+    String getTribulaName();
     double getTribulaNextDamage();
     LivingEntity getDisplayMob();
-    int getLevel();
+    int getTribulaLevel();
     int getTribulaHealth();
     int getTribulaMaxHealth();
     void getSpecial(); //TODO
